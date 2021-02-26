ff0d4d0c023c1b47b257f9ed9f4b43852ad09423
Alif Ananda <75667307+ExplodedRiot@users.noreply.github.com>
2021-02-26 16:49:05 +0700

    Add files via upload

---
+++ Jobsheet2/AboutController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about(){
+        echo "My Name is Muhammad Alif Ananda, and My NIM is 1941720078"
+    }
+}
